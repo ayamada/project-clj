867d1d22136b3f949cb6f931b764d97a7fbc7a01
atsuo yamada <yamada-git@tir.jp>
2015-04-05 23:10:46 +0900

    Released 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jp.ne.tir/project-clj "0.1.0"
+(defproject jp.ne.tir/project-clj "0.1.1-SNAPSHOT"
   :description "Refer own project.clj"
   :url "https://github.com/ayamada/project-clj"
   :license {:name "Unlicense"
